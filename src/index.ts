58295887c5100efaae7fe6003b78f816b785f897
Mik13 <Mik13@users.noreply.github.com>
2022-03-31 21:28:04 +0200

    Jitter-Finger-Fix

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,58 @@
 import { VNode, VNodeDirective } from 'vue/types/vnode'
 
 interface LongPressHTMLElement extends HTMLElement {
-  $_long_press_pointerdown_handler : () => void;
-  dataset : {
-    longPressTimeoutId : string;
-  };
+    $_long_press_pointerdown_handler : () => void;
+    $_long_press_top : number;
+    dataset : {
+        longPressTimeoutId : string;
+    };
 }
 
 const longPressStop = new CustomEvent('long-press-stop')
 const longPressStart = new CustomEvent('long-press-start')
 
 export const directiveOption = {
-  bind (el: LongPressHTMLElement, binding: VNodeDirective, vnode: VNode) {
-    el.dataset.longPressTimeoutId = '0'
+    bind (el: LongPressHTMLElement, binding: VNodeDirective, vnode: VNode) {
+        el.dataset.longPressTimeoutId = '0'
 
-    const onpointerup = () => {
-      clearTimeout(parseInt(el.dataset.longPressTimeoutId))
+        const onpointerup = () => {
+            clearTimeout(parseInt(el.dataset.longPressTimeoutId))
 
-      if (vnode.componentInstance) {
-        vnode.componentInstance.$emit('long-press-stop')
-      } else {
-        el.dispatchEvent(longPressStop)
-      }
+            if (Math.abs(el.getBoundingClientRect().top - el.$_long_press_top) < 10) {
+                if (vnode.componentInstance) {
+                    vnode.componentInstance.$emit('long-press-stop')
+                } else {
+                    el.dispatchEvent(longPressStop)
+                }
+            }
 
-      document.removeEventListener('pointerup', onpointerup)
-    }
-
-    const onpointerdown = () => {
-      document.addEventListener('pointerup', onpointerup)
-
-      const timeout = setTimeout(() => {
-        if (vnode.componentInstance) {
-          vnode.componentInstance.$emit('long-press-start')
-        } else {
-          el.dispatchEvent(longPressStart)
+            document.removeEventListener('pointerup', onpointerup)
         }
-      }, binding.value)
 
-      el.dataset.longPressTimeoutId = timeout.toString()
+        const onpointerdown = () => {
+            document.addEventListener('pointerup', onpointerup)
+            el.$_long_press_top = el.getBoundingClientRect().top
+
+            const timeout = setTimeout(() => {
+                if (Math.abs(el.getBoundingClientRect().top - el.$_long_press_top) < 10) {
+                    if (vnode.componentInstance) {
+                        vnode.componentInstance.$emit('long-press-start')
+                    } else {
+                        el.dispatchEvent(longPressStart)
+                    }
+                }
+            }, binding.value)
+
+            el.dataset.longPressTimeoutId = timeout.toString()
+        }
+
+        el.$_long_press_pointerdown_handler = onpointerdown;
+        el.addEventListener('pointerdown', onpointerdown)
+    },
+    unbind (el : LongPressHTMLElement) {
+        clearTimeout(parseInt(el.dataset.longPressTimeoutId))
+        el.removeEventListener('pointerdown', el.$_long_press_pointerdown_handler)
     }
-
-    el.$_long_press_pointerdown_handler = onpointerdown;
-    el.addEventListener('pointerdown', onpointerdown)
-  },
-  unbind (el : LongPressHTMLElement) {
-    clearTimeout(parseInt(el.dataset.longPressTimeoutId))
-    el.removeEventListener('pointerdown', el.$_long_press_pointerdown_handler)
-  }
 }
 
 export default directiveOption
